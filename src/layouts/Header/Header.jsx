b629f2b5a4e54d92bda215866904504046c048c0
varivonchik-artem <varivonchik.core@gmail.com>
2025-12-26 14:23:51 +0300

    feat(layouts): implement adaptive header component

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -30,39 +30,41 @@ export const Header = (props) => {
     <header className="header">
       <div className="header__inner container">
         <Logo className="header__logo" loading="eager" />
-        <nav className="header__menu">
-          <ul className="header__menu-list">
-            {menuItems.map(({ label, href }, index) => (
-              <li className="header__menu-item" key={index}>
-                <a
-                  className={clx("header__menu-link", {
-                    "header__menu-link_active": href === url,
-                  })}
-                  href={href}
-                >
-                  {label}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <div className="header__actions">
-          <Button
-            className="header__button"
-            label="Search"
-            isLabelHidden
-            iconName="search"
-            mode="transparent"
-          />
-          <Button
-            className="header__button"
-            label="Notifications"
-            isLabelHidden
-            iconName="notification"
-            mode="transparent"
-          />
-        </div>
-        <BurgerButton />
+        <dialog className="header__overlay-menu-dialog" open>
+          <nav className="header__menu">
+            <ul className="header__menu-list">
+              {menuItems.map(({ label, href }, index) => (
+                <li className="header__menu-item" key={index}>
+                  <a
+                    className={clx("header__menu-link", {
+                      "header__menu-link_active": href === url,
+                    })}
+                    href={href}
+                  >
+                    {label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div className="header__actions">
+            <Button
+              className="header__button"
+              label="Search"
+              isLabelHidden
+              iconName="search"
+              mode="transparent"
+            />
+            <Button
+              className="header__button"
+              label="Notifications"
+              isLabelHidden
+              iconName="notification"
+              mode="transparent"
+            />
+          </div>
+        </dialog>
+        <BurgerButton className="header__burger-button" />
       </div>
     </header>
   )
