8263dbf7803af2da235ee842c4329aefd4af6778
varivonchik-artem <varivonchik.core@gmail.com>
2026-01-07 20:31:44 +0300

    feat(widgets): create adaptive footer widget

---
+++ src/shared/ui/Socials/Socials.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import { Button } from "@shared/ui/Button";
+
+import clx from "classnames";
+import "./Socials.scss";
+
+interface SocialLink {
+  label: string;
+  iconName: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  href?: string;
+}
+
+interface SocialsProps {
+  className?: string;
+
+  links?: SocialLink[];
+}
+
+export const Socials = (props: SocialsProps) => {
+  const { className, links } = props;
+
+  return (
+    <div className={clx(className, "soc1als")}>
+      <ul className="soc1als__list">
+        {links.map(({ label, iconName }, index) => (
+          <li className="soc1als__item" key={index}>
+            <Button
+              className="soc1als_link"
+              mode="black-10"
+              label={label}
+              isLabelVisible={false}
+              icon={iconName}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
